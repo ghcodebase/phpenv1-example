18a1593202d6d1c77e4d149e5cf168fdd2a60693
ghcodebase <inet01@yahoo.co.uk>
2016-01-13 22:09:51 +0000

    Added CSS to project

--- public/index.php
+++ public/index.php
@@ -1,5 +1,6 @@
 <?php
 
+use Particle\Validator\Validator;
 require_once '../vendor/autoload.php';
 
 
@@ -13,11 +14,35 @@ $database = new medoo([
 ]);
 
 $comment = new SitePoint\Comment($database);
-$comment->setEmail('bruno@skvorc.me')
-  ->setName('Bruno Skvorc')
-//  ->setComment('It works!')  This line in the book creates an error, as it is too short for the requirement set on line 55 of Comment.php.  It is also overwritten by the following comment.
-  ->setComment('Hooray! Saving comments works!')
-  ->save();
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  $v = new Validator();
+  $v->required('name')->lengthBetween(1, 100)->alnum(true);
+  $v->required('email')->email()->lengthBetween(5, 255);
+  $v->required('comment')->lengthBetween(10, null);
+
+  $result = $v->validate($_POST);
+  if ($result->isValid()) {
+
+    try {
+      $comment
+        ->setName($_POST['name'])
+        ->setEmail($_POST['email'])
+        ->setComment($_POST['comment'])
+        ->save();
+
+      header('Location: /');
+      return;
+
+    } catch (\Exception $e) {
+      die($e->getMessage());
+    }
+
+  } else {
+    dump($result->getMessages());
+  }
+
+}
 
 // Viewing the output from the database
 // dump($database);
@@ -38,6 +63,7 @@ $comment->setEmail('bruno@skvorc.me')
 
         <link rel="stylesheet" href="css/normalize.css">
         <link rel="stylesheet" href="css/main.css">
+        <link rel="stylesheet" href="css/custom.css">
         <script src="js/vendor/modernizr-2.8.3.min.js"></script>
     </head>
     <body>
@@ -46,7 +72,19 @@ $comment->setEmail('bruno@skvorc.me')
         <![endif]-->
 
         <!-- Add your site or application content here -->
-        <form method="post">
+      <?php foreach ($comment->findAll() as $comment) : ?>
+
+        <div class="comment">
+          <h3>On <?= $comment->getSubmissionDate() ?>, <?= $comment->getName() ?>
+            wrote:</h3>
+          <p><?= $comment->getComment(); ?></p>
+
+        </div>
+      <?php endforeach; ?>
+
+
+
+      <form method="post">
           <label>Name: <input type="text" name="name" placeholder="Your name"></label>
           <label>Email: <input type="text" name="email" placeholder="your@email.com"></label>
           <label>Comment: <textarea name="comment" cols="30" rows="10"></textarea></label>
